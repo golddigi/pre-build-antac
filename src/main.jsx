413c7dc99048327c9d940b5c50eb1bffe18b6619
edge0024 <apu7rv@gmail.com>
2025-12-03 12:00:35 +0530

    Fix router reload issue and clean project

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ScrollToTop from "./components/shared/ScrollToTop.jsx";
@@ -6,8 +6,8 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ScrollToTop />
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
